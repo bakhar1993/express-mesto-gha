345a8af965d7516231c859413dea70360e17ff21
Алексей Бахарев <bakhar1993@gmail.com>
2022-06-20 21:55:19 +0300

    Исправление ошибок после первого ревью

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,33 +5,39 @@ module.exports.getCards = (req, res, next) => {
     if (data.length >= 1) {
       res.send({ cards: data });
     } else {
-      res.status(400).send({ message: 'Карточки не найдены' });
+      res.status(200).send({ message: 'Карточки не найдены' });
     }
   }).catch(next);
 };
 
-module.exports.createCard = (req, res, next) => {
+module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
-  if (!name || !link) {
-    res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
-  }
   Card.create({ name, link, owner: req.user._id }).then((data) => {
     res.send({ card: data });
-  }).catch(next);
+  }).catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' });
+    } else {
+      res.status(500).send('Ошибка по умолчанию');
+    }
+  });
 };
 
-module.exports.deleteCard = (req, res, next) => {
+module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId).then((data) => {
     if (data) {
       res.send({ card: data });
     } else { res.status(404).send({ message: 'Карточка с указанным _id не найдена' }); }
-  }).catch(() => {
-    res.status(500);
-    next();
+  }).catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(400).send({ message: 'Переданы некорректные данные при удалении карточки' });
+    } else {
+      res.status(500).send('Ошибка по умолчанию');
+    }
   });
 };
 
-module.exports.likeCard = (req, res, next) => {
+module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
@@ -44,13 +50,12 @@ module.exports.likeCard = (req, res, next) => {
     if (err.name === 'CastError') {
       res.status(400).send({ message: 'Переданы некорректные данные для постановки лайка' });
     } else {
-      res.status(500);
-      next();
+      res.status(500).send('Ошибка по умолчанию');
     }
   });
 };
 
-module.exports.dislikeCard = (req, res, next) => {
+module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
@@ -63,8 +68,7 @@ module.exports.dislikeCard = (req, res, next) => {
     if (err.name === 'CastError') {
       res.status(400).send({ message: 'Переданы некорректные данные для снятии лайка' });
     } else {
-      res.status(500);
-      next();
+      res.status(500).send('Ошибка по умолчанию');
     }
   });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,18 @@
 const User = require('../models/user');
 
-module.exports.getUser = (req, res, next) => {
+module.exports.getUser = (req, res) => {
   User.find({}).then((data) => {
     if (data.length >= 1) {
       res.send({ users: data });
     } else {
-      res.status(400).send({ message: 'Пользователи не найдены' });
+      res.status(200).send({ message: 'Пользователи не найдены' });
     }
   }).catch(() => {
-    res.status(500);
-    next();
+    res.status(500).send('Ошибка по умолчанию');
   });
 };
 
-module.exports.getUserById = (req, res, next) => {
+module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId).then((data) => {
     if (data) {
       res.send({ user: data });
@@ -22,27 +21,21 @@ module.exports.getUserById = (req, res, next) => {
     }
   }).catch((err) => {
     if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Некорректный ID' });
-      next();
+      res.status(404).send({ message: 'Некорректный ID' });
     }
   });
 };
 
-module.exports.createUser = (req, res, next) => {
+module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  // if (!name || !about || !avatar) {
-  //   res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
-  // }
-
   User.create({ name, about, avatar }).then((user) => {
     res.send({ user });
   }).catch(() => {
     res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
-    next();
   });
 };
 
-module.exports.updateUser = (req, res, next) => {
+module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about }, {
     new: true,
@@ -54,16 +47,15 @@ module.exports.updateUser = (req, res, next) => {
       res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
     }
   }).catch((err) => {
-    if (err.name === 'ValidationError') {
+    if (err.name === 'ValidationError' || err.name === 'CastError') {
       res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
     } else {
-      res.status(500);
-      next();
+      res.status(500).send('Ошибка по умолчанию');
     }
   });
 };
 
-module.exports.updateAvatar = (req, res, next) => {
+module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, {
     new: true,
@@ -75,11 +67,10 @@ module.exports.updateAvatar = (req, res, next) => {
       res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
     }
   }).catch((err) => {
-    if (err.name === 'ValidationError') {
+    if (err.name === 'ValidationError' || err.name === 'CastError') {
       res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' });
     } else {
-      res.status(500);
-      next();
+      res.status(500).send('Ошибка по умолчанию');
     }
   });
 };
